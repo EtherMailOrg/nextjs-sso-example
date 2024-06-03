e70eccc7cf4431b054e071347c9a12b3947d2074
Sebas Arango <sebastianarango201316@hotmail.com>
2024-06-03 09:52:58 +0200

    Signed from utils

--- src/app/token/page.tsx
+++ src/app/token/page.tsx
@@ -2,28 +2,12 @@
 import ethers, { BrowserProvider } from 'ethers';
 import { Toaster, toast } from "react-hot-toast";
 import { useAppSelector } from "@/lib/hooks";
+import { Web3Utils } from "@/utils/web3.utils";
 
 export default function TokenPage() {
   const web3Provider = useAppSelector(state => state.web3Provider.value) as BrowserProvider | undefined;
   const ethermailProvider = useAppSelector(state => state.ethermailProvider.value) as BrowserProvider | undefined;
-
-  async function handleSignMessage() {
-    try {
-      if (!web3Provider) throw Error("Need provider to sign!");
-
-      const message = "Test message to sign!";
-      const signer = await web3Provider.getSigner();
-
-      if (!signer) throw Error("No signer!");
-
-      const signedMessage = await signer.signMessage(message);
-      toast.success(`Signed Message: ${signedMessage}`);
-    } catch(err: any) {
-      toast.error(err.message);
-      console.log(err);
-    }
-  }
-
+  const web3Utils = new Web3Utils();
 
   return (
     <>
@@ -38,11 +22,11 @@ export default function TokenPage() {
                   <h1>Actions:</h1>
                   <div>
                     <h4>Sign Message:</h4>
-                    <button onClick={handleSignMessage}>Sign Message</button>
+                    <button onClick={async () => {await web3Utils.handleSignMessage(web3Provider)}}>Sign Message</button>
                   </div>
                   <div>
                     <h4>Claim Token:</h4>
-                    <button onClick={handleSignMessage}>Claim Token</button>
+                    <button onClick={async () => {await web3Utils.handleSignMessage(web3Provider)}}>Claim Token</button>
                   </div>
                 </div>
               </div>
