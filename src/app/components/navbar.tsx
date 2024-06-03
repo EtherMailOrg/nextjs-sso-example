e052bbc06095f97a76e5835652acba6d3b06e1e7
Sebas Arango <sebastianarango201316@hotmail.com>
2024-06-03 16:57:18 +0200

    Added listener on EtherMail Provider

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -45,6 +45,14 @@ export default function NavBar() {
       });
       const __browserProvider = new BrowserProvider(__ethermailProvider);
 
+      __ethermailProvider.on("disconnect", () => {
+        toast("Disconnect event heard!");
+      });
+
+      __ethermailProvider.on("chainChanged", () => {
+        toast("Changed chain event heard!");
+      });
+
       __browserProvider.on("disconnect", () => {
         toast("Disconnect event heard!");
       });
