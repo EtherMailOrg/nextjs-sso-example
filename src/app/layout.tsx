8b937513c89ed5f63543c63a55afeb5d1c87edb6
Shant Kevonian <shant.kevonian@gmail.com>
2024-10-18 16:57:22 +0200

    use client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,47 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import NavBar from "@/app/components/navbar";
-import StoreProvider from "@/app/StoreProvider";
+'use client';
 
-const inter = Inter({ subsets: ["latin"] });
+import { Inter } from 'next/font/google';
+import './globals.css';
+import NavBar from '@/app/components/navbar';
+import StoreProvider from '@/app/StoreProvider';
+import { useEffect } from 'react';
 
-export const metadata: Metadata = {
-  title: "Ethermail SSO Test",
-  description: "This was created to test SSO by EtherMail",
-};
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    (function({ ...args }) {
+      if (!document.getElementById('ethermail-sdk-script')) {
+        var p = document.createElement('script');
+        p.id = 'ethermail-sdk-script';
+        p.src = 'https://cdn-email.ethermail.io/sdk/v2/staging-ethermail.js';
+        document.body.appendChild(p);
+        p.setAttribute('a', args.afid);
+        p.setAttribute('b', args.communityAlias);
+        // @ts-ignore
+        p.setAttribute('c', args.features);
+      }
+    })({
+      afid: '65df72841e9e35f978ba45f5',
+      communityAlias: 'ethermail',
+      features: ['login'],
+    });
+  }, []);
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <StoreProvider>
-        <div>
-          <NavBar />
-          {children}
-        </div>
-      </StoreProvider>
-      </body>
+    <body className={inter.className}>
+    <StoreProvider>
+      <div>
+        <NavBar />
+        {children}
+      </div>
+    </StoreProvider>
+    </body>
     </html>
   );
 }
