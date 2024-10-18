91321bafb3098085ce837d8b95b608af13e360e9
Shant Kevonian <shant.kevonian@gmail.com>
2024-10-18 16:43:39 +0200

    add login type

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,23 +1,23 @@
-"use client"
+'use client';
 
-import { useEffect, useState } from "react";
-import { toast } from "react-hot-toast";
+import { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
 import {
   EtherMailSignInOnSuccessEvent,
   EtherMailTokenErrorEvent,
-  SSOPermissionType
-} from "@/intefaces/web3.interfaces";
-import jwt from "jsonwebtoken";
-import { EtherMailProvider } from "@ethermail/ethermail-wallet-provider";
-import { BrowserProvider } from "ethers";
-import { useAppDispatch, useAppSelector } from "@/lib/hooks";
-import { _web3Provider, web3ProviderSlice } from "@/lib/reducers/web3ProviderSlice";
-import { _ethermailProvider } from "@/lib/reducers/ethermailProviderSlice";
-import { _loginDataProvider } from "@/lib/reducers/loginDataProviderSlice";
-import Script from "next/script";
-import { Chain } from "@/intefaces/web3.interfaces";
-import { usePathname, useRouter } from "next/navigation";
-import { EthermailLoginData } from "@/intefaces/ethermail.interfaces";
+  SSOPermissionType,
+} from '@/intefaces/web3.interfaces';
+import jwt from 'jsonwebtoken';
+import { EtherMailProvider } from '@ethermail/ethermail-wallet-provider';
+import { BrowserProvider } from 'ethers';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { _web3Provider, web3ProviderSlice } from '@/lib/reducers/web3ProviderSlice';
+import { _ethermailProvider } from '@/lib/reducers/ethermailProviderSlice';
+import { _loginDataProvider } from '@/lib/reducers/loginDataProviderSlice';
+import Script from 'next/script';
+import { Chain } from '@/intefaces/web3.interfaces';
+import { usePathname, useRouter } from 'next/navigation';
+import { EthermailLoginData } from '@/intefaces/ethermail.interfaces';
 
 export default function NavBar() {
   const router = useRouter();
@@ -28,33 +28,37 @@ export default function NavBar() {
   const loginData = useAppSelector(state => state.loginData.value) as EthermailLoginData | undefined;
   const dispatch = useAppDispatch();
 
-  const [ssoPermission, setSsoPermission] = useState("write");
-  const [chains, setChains] = useState<Chain[]>([{ name: "Ethereum", chainId: 1 }, { name: "Polygon", chainId: 137 }, { name: "Arbitrum", chainId: 42161 }]);
+  const [ssoPermission, setSsoPermission] = useState('write');
+  const [chains, setChains] = useState<Chain[]>([{ name: 'Ethereum', chainId: 1 }, {
+    name: 'Polygon',
+    chainId: 137,
+  }, { name: 'Arbitrum', chainId: 42161 }]);
   const [chain, setChain] = useState<Chain | undefined>();
+  const [loginType, setLoginType] = useState('wallet');
 
   useEffect(() => {
-    toast.success("Setting Event Listeners!");
-    window.addEventListener("EtherMailSignInOnSuccess",  (event) => {
+    toast.success('Setting Event Listeners!');
+    window.addEventListener('EtherMailSignInOnSuccess', (event) => {
       const __loginEvent = event as EtherMailSignInOnSuccessEvent;
       const __loginData = jwt.decode(__loginEvent.detail.token);
       console.log(__loginData);
 
       const __ethermailProvider = new EtherMailProvider({
-        websocketServer: "wss://staging-api.ethermail.io/events",
-        appUrl: "https://staging.ethermail.io"
+        websocketServer: 'wss://staging-api.ethermail.io/events',
+        appUrl: 'https://staging.ethermail.io',
       });
       const __browserProvider = new BrowserProvider(__ethermailProvider);
 
-      __ethermailProvider.on("disconnect", () => {
-        toast("Disconnect event heard!");
+      __ethermailProvider.on('disconnect', () => {
+        toast('Disconnect event heard!');
       });
 
-      __ethermailProvider.on("chainChanged", () => {
-        toast("Changed chain event heard!");
+      __ethermailProvider.on('chainChanged', () => {
+        toast('Changed chain event heard!');
       });
 
-      __ethermailProvider.on("message", () => {
-        toast("Message event heard!");
+      __ethermailProvider.on('message', () => {
+        toast('Message event heard!');
       });
 
       dispatch(_loginDataProvider.setData(__loginData as EthermailLoginData));
@@ -62,13 +66,13 @@ export default function NavBar() {
       dispatch(_web3Provider.setProvider(__browserProvider));
     });
 
-    window.addEventListener("EtherMailTokenError", (event: Event) => {
+    window.addEventListener('EtherMailTokenError', (event: Event) => {
       console.log(event);
       const errorEvent = event as EtherMailTokenErrorEvent;
-      if (errorEvent.detail.type === "expired") {
-        toast.error("Expired Session!");
-      } else if (errorEvent.detail.type === "permissions") {
-        toast.error("Permissions Error!");
+      if (errorEvent.detail.type === 'expired') {
+        toast.error('Expired Session!');
+      } else if (errorEvent.detail.type === 'permissions') {
+        toast.error('Permissions Error!');
       }
     });
   }, []);
@@ -77,12 +81,12 @@ export default function NavBar() {
     try {
       const selectedValue = event.target.value;
 
-      if (!selectedValue) throw Error("Must select a valid permission");
+      if (!selectedValue) throw Error('Must select a valid permission');
 
       setSsoPermission(selectedValue as SSOPermissionType);
       router.refresh();
-      toast.success("SSO Permissions Changed!");
-    } catch(err: any) {
+      toast.success('SSO Permissions Changed!');
+    } catch (err: any) {
       toast.error(err.message);
       console.log(err);
     }
@@ -92,17 +96,17 @@ export default function NavBar() {
     try {
       const chainId = +event.target.value;
 
-      if (!ethermailProvider) throw new Error("Connect wallet before");
+      if (!ethermailProvider) throw new Error('Connect wallet before');
 
       await ethermailProvider.request({
-        method: "wallet_switchEthereumChain",
-        params: [{ chainId: chainId }]
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: chainId }],
       });
 
       setChain(chains.find(chain => chain.chainId === chainId) ?? undefined);
-      toast.success("Chain changed!");
-    } catch(err: any) {
-      event.target.value = "";
+      toast.success('Chain changed!');
+    } catch (err: any) {
+      event.target.value = '';
       toast.error(err.message);
       console.log(err);
     }
@@ -110,7 +114,7 @@ export default function NavBar() {
 
   async function handleDisconnect() {
     try {
-      if (!ethermailProvider) throw new Error("No Provider!");
+      if (!ethermailProvider) throw new Error('No Provider!');
 
 
       await (ethermailProvider as EtherMailProvider).disconnect();
@@ -118,7 +122,7 @@ export default function NavBar() {
       dispatch(_web3Provider.reset());
       dispatch(_ethermailProvider.reset());
       dispatch(_loginDataProvider.reset());
-      toast.success("Wallet disconnected!");
+      toast.success('Wallet disconnected!');
     } catch (err: any) {
       toast.error(err.message);
       console.log(err);
@@ -129,13 +133,17 @@ export default function NavBar() {
     try {
       const newPage = event.target.value;
 
-      if (newPage === path) throw new Error("Already on selected page");
+      if (newPage === path) throw new Error('Already on selected page');
 
       router.replace(newPage);
     } catch (err: any) {
       console.log(err);
       toast.error(err.message);
     }
+  }
+
+  function handleLoginTypeChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    setLoginType(event.target.value);
   }
 
   return (
@@ -155,7 +163,7 @@ export default function NavBar() {
           })({
             afid: '65ddf7aa3631bb310429bbb7',
             communityAlias: 'prestige-worldwid',
-            features: ['login']
+            features: ['login'],
           });
         }}
       />
@@ -168,20 +176,20 @@ export default function NavBar() {
             <select onChange={handlePageChange}>
               <option key="labelOption" value="">--- Change Page ---</option>
               {
-                ["/", "/token", "/nfts"].map(page => {
-                  return <option key={"page-" + page}
-                                 value={page}>{page === "/" ? "Home" : page.slice(1)}{page === path ? " (Current)" : ""}</option>;
+                ['/', '/token', '/nfts'].map(page => {
+                  return <option key={'page-' + page}
+                                 value={page}>{page === '/' ? 'Home' : page.slice(1)}{page === path ? ' (Current)' : ''}</option>;
                 })
               }
             </select>
             {loginData ?
-              "" :
+              '' :
               <select onChange={handleSSOPermissionChange}>
                 <option key="labelOption" value="">--- Change SSO Permissions ---</option>
                 {
-                  ["write", "read", "none"].map(permission => {
-                    return <option key={"ssoOption-" + permission}
-                                   value={permission}>{permission}{ssoPermission === permission ? " (Current)" : ""}</option>;
+                  ['write', 'read', 'none'].map(permission => {
+                    return <option key={'ssoOption-' + permission}
+                                   value={permission}>{permission}{ssoPermission === permission ? ' (Current)' : ''}</option>;
                   })
                 }
               </select>
@@ -190,10 +198,14 @@ export default function NavBar() {
               <option key="labelOption" value="">--- Change Chain ---</option>
               {
                 chains.map(_chain => {
-                  return <option key={"chain-" + _chain.chainId}
-                                 value={_chain.chainId}>{_chain.name + "-" + _chain.chainId}{_chain.chainId === chain?.chainId ? " (Current)" : ""}</option>;
+                  return <option key={'chain-' + _chain.chainId}
+                                 value={_chain.chainId}>{_chain.name + '-' + _chain.chainId}{_chain.chainId === chain?.chainId ? ' (Current)' : ''}</option>;
                 })
               }
+            </select>
+            <select onChange={handleLoginTypeChange} value={loginType}>
+              <option value="wallet">Wallet</option>
+              <option value="sso">SSO</option>
             </select>
           </div>
           <div>
@@ -210,12 +222,18 @@ export default function NavBar() {
                 <button onClick={handleDisconnect}>Disconnect</button>
               </div>
               :
-              <ethermail-login style={{width: "150px", backgroundColor: "black", color: "white", fontWeight: 700, borderRadius: "1rem"}} widget="6659a4865f3bb424d99d11b2" type="wallet"
+              <ethermail-login style={{
+                width: '150px',
+                backgroundColor: 'black',
+                color: 'white',
+                fontWeight: 700,
+                borderRadius: '1rem',
+              }} widget="6659a4865f3bb424d99d11b2" type={loginType}
                                permissions={ssoPermission} label="Connect Wallet"></ethermail-login>
             }
           </div>
         </div>
       </nav>
     </>
-  )
+  );
 }
